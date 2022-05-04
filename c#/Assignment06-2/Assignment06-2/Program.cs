4414f7ea29b3c1401d87908fd0cef5cb8b3e34e3
Vinodkumar Bantu <v.bantu1@zensar.com>
2022-05-04 21:00:08 +0530

    c#Assignmeent06

---
+++ c#/Assignment06-2/Assignment06-2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment06_2
+{
+     //-------Books-----//
+    struct Books
+    {
+        public string Name;
+        private int BookId { get; set; }
+
+        public int BookID
+        {
+            get
+            {
+                return BookId;
+            }
+            set
+            {
+                BookId = value;
+            }
+        }
+        public void Display()
+        {
+            Console.WriteLine("Book Id Is :{0}", BookId);
+            Console.WriteLine("Book name is :{0}", Name);
+        }
+
+
+    }
+    class StructEg1
+    {
+        static void Main()
+        {
+            Books book = new Books();
+            book.BookID = 0007;
+            book.Name = "Atomic Habits";
+            book.Display();
+            Console.ReadLine();
+        }
+    }
+}
