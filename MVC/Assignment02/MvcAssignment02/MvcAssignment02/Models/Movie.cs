26d01d2326414827ded9ce348ed0703c511d9b66
Vinodkumar Bantu <v.bantu1@zensar.com>
2022-06-11 23:26:17 +0530

    MvcAssignments

---
+++ MVC/Assignment02/MvcAssignment02/MvcAssignment02/Models/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcAssignment02.Models
+{
+    [Table("TblMovie")]
+    public class Movie
+    {
+        [Key]
+        public int MovieId { get; set; }
+        public string MovieName { get; set; }
+        public Nullable<System.DateTime> ReleaseDate { get; set; }
+    }
+}
---
+++ MVC/Assignment02/MvcAssignment02/MvcAssignment02/Models/MovieContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+namespace MvcAssignment02.Models
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext() : base("Name=MovieEntites")
+        {
+
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
